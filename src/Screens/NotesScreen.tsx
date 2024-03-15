4fc6a33946c14a9cef0f8f39e556b4c930d7b99d
Chuck Russell <chuckjohnrussell@gmail.com>
2024-03-16 00:26:49 +1100

    Added the mobile/xs nav screen

--- src/Screens/NotesScreen.tsx
+++ src/Screens/NotesScreen.tsx
@@ -1,6 +1,6 @@
 import {StackNavigationProp} from '@react-navigation/stack';
 import {RootStackParamList} from '../Navigation/NavigationTypes';
-import {ScreenWrapper, Typography} from '@ui-library/atoms';
+import {Button, ScreenWrapper, Typography} from '@ui-library/atoms';
 import {signOut} from 'firebase/auth';
 import {auth} from '../firebase/firebase.config';
 import {View} from 'react-native';
@@ -8,16 +8,13 @@ import {DEVICE_SIZES, minSize, useSizeRender} from 'rn-responsive-styles';
 import {CreateThemedStyle} from '@ui-library/context/theme';
 import {ScreenHeaderFilters} from '@ui-library/organisms/ScreenHeaderFilter';
 import {EditableMarkdownView} from '@ui-library/molecules';
-import {initialNote} from '../mocks/data';
-import {useEffect, useState} from 'react';
+import {useState} from 'react';
 import {RouteProp} from '@react-navigation/native';
-import firestore from '../firebase/firestore';
-import {useUserProvider} from '../context/UserProvider';
-import {NoteModel} from '../firebase/firestoreTypes';
 import {ActiveNotePanel} from './Notes/ActiveNotesPanel';
 import {PanelFooterButton} from './Notes/PanelFooterButton';
 import {Panel} from './Notes/Panel';
 import {NoteSelectionPanel} from './Notes/NoteSelectionPanel';
+import {Drawer} from 'react-native-drawer-layout';
 
 interface NotesScreenProps {
   navigation: StackNavigationProp<RootStackParamList, 'Notes'>;
@@ -28,6 +25,7 @@ export const NotesScreen = ({navigation, route}: NotesScreenProps) => {
   const styles = themedStyles();
   const {isSmallerThan, isLargerThan, isSize} = useSizeRender();
   const [isSessionActive, setIsSessionActive] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const {noteId: selectedNoteId, campaignId: selectedCampaignId} =
     route.params || {}; //sad that I need to do this :(
@@ -42,70 +40,110 @@ export const NotesScreen = ({navigation, route}: NotesScreenProps) => {
     return <Typography variant="heading1">No Campaign</Typography>;
 
   return (
-    <ScreenWrapper>
-      <ScreenHeaderFilters
-        tags={['Character', 'Item', 'Location']}
-        onSignout={() => {
-          signOut(auth);
-        }}
-      />
-      <View style={styles.pageWrapper}>
-        {isLargerThan(DEVICE_SIZES.MD) ? (
-          <NoteSelectionPanel
-            onNoteCreated={newNote => {
-              navigation.setParams({
-                noteId: newNote.id,
-              });
-            }}
-            onSelectedNoteChanged={selectedNote => {
-              navigation.setParams({
-                noteId: selectedNote.id,
-              });
-            }}
-            selectedNoteId={selectedNoteId}
-            campaignId={selectedCampaignId}
-          />
-        ) : null}
-        {/* Active Notes Panel: Should NOT render if there is no content (no selected id) 
+    <Drawer
+      open={open}
+      drawerStyle={styles.drawerStyle}
+      onOpen={() => setOpen(true)}
+      onClose={() => setOpen(false)}
+      overlayStyle={{backgroundColor: 'transparent'}}
+      renderDrawerContent={() => {
+        return (
+          <>
+            <Button
+              variant="secondary"
+              text="Close"
+              onPress={() => setOpen(false)}></Button>
+            <NoteSelectionPanel
+              onNoteCreated={newNote => {
+                navigation.setParams({
+                  noteId: newNote.id,
+                });
+              }}
+              onSelectedNoteChanged={selectedNote => {
+                navigation.setParams({
+                  noteId: selectedNote.id,
+                });
+                setOpen(false);
+              }}
+              selectedNoteId={selectedNoteId}
+              campaignId={selectedCampaignId}
+            />
+          </>
+        );
+      }}>
+      <ScreenWrapper>
+        <ScreenHeaderFilters
+          tags={['Character', 'Item', 'Location']}
+          onMenuPressed={() => {
+            setOpen(true);
+          }}
+          onSignout={() => {
+            signOut(auth);
+          }}
+        />
+        <View style={styles.pageWrapper}>
+          {isLargerThan(DEVICE_SIZES.MD) ? (
+            <NoteSelectionPanel
+              onNoteCreated={newNote => {
+                navigation.setParams({
+                  noteId: newNote.id,
+                });
+              }}
+              onSelectedNoteChanged={selectedNote => {
+                navigation.setParams({
+                  noteId: selectedNote.id,
+                });
+              }}
+              selectedNoteId={selectedNoteId}
+              campaignId={selectedCampaignId}
+            />
+          ) : null}
+          {/* Active Notes Panel: Should NOT render if there is no content (no selected id) 
           Also should not render if its in two panel layout and not active
         */}
-        {showNotesPanel && (
-          <ActiveNotePanel
-            noteId={selectedNoteId}
-            campaignId={selectedCampaignId}
-            onSessionPress={() => setIsSessionActive(true)}
-          />
-        )}
+          {showNotesPanel && (
+            <ActiveNotePanel
+              noteId={selectedNoteId}
+              campaignId={selectedCampaignId}
+              onSessionPress={() => setIsSessionActive(true)}
+            />
+          )}
 
-        {/* Session Notes Panel */}
+          {/* Session Notes Panel */}
 
-        {showSessionPanel && (
-          <Panel isContent>
-            <View style={{flexGrow: 1}}>
-              <Typography
-                style={{paddingHorizontal: 20, marginBottom: 20}}
-                variant="heading2">
-                Session Notes
-              </Typography>
-              <View style={{margin: 20}}>
-                <EditableMarkdownView />
+          {showSessionPanel && (
+            <Panel isContent>
+              <View style={{flexGrow: 1}}>
+                <Typography
+                  style={{paddingHorizontal: 20, marginBottom: 20}}
+                  variant="heading2">
+                  Session Notes
+                </Typography>
+                <View style={{margin: 20}}>
+                  <EditableMarkdownView />
+                </View>
               </View>
-            </View>
-            {isSmallerThan(DEVICE_SIZES.XL) && (
-              <PanelFooterButton
-                text="Switch to Note"
-                onPress={() => setIsSessionActive(false)}
-              />
-            )}
-          </Panel>
-        )}
-      </View>
-    </ScreenWrapper>
+              {isSmallerThan(DEVICE_SIZES.XL) && (
+                <PanelFooterButton
+                  text="Switch to Note"
+                  onPress={() => setIsSessionActive(false)}
+                />
+              )}
+            </Panel>
+          )}
+        </View>
+      </ScreenWrapper>
+    </Drawer>
   );
 };
 
 const themedStyles = CreateThemedStyle(theme => ({
   defaultStyle: {
+    drawerStyle: {
+      backgroundColor: theme.application.backgroundColor,
+      width: '100%',
+      padding: theme.application.padding,
+    },
     pageWrapper: {
       flexDirection: 'row',
       gap: theme.panel.gap,
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import {defineConfig, transformWithEsbuild} from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import {extname} from 'path';
 import svgr from 'vite-plugin-svgr';
+import babel from 'vite-plugin-babel';
 
 export default defineConfig({
   define: {
@@ -13,6 +15,7 @@ export default defineConfig({
     esbuildOptions: {
       mainFields: ['module', 'main'],
       resolveExtensions: ['.web.js', '.js', '.ts'],
+      jsx: 'automatic',
       loader: {
         '.js': 'jsx',
       },
@@ -42,6 +45,18 @@ export default defineConfig({
     },
     svgr(),
     react(),
+    babel({
+      filter:
+        /[\/](node_modules\/@react-navigation|node_modules\/react-native-drawer-layout)[\/].+\.(js|ts|jsx|tsx)$/,
+      babelConfig: {
+        babelrc: false,
+        configFile: false,
+        plugins: [
+          '@babel/plugin-proposal-export-namespace-from',
+          'react-native-reanimated/plugin',
+        ],
+      },
+    }),
   ],
   build: {
     commonjsOptions: {
